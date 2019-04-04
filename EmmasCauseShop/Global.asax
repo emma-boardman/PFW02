710c9afac6735c6abd00b6086109915dd6f16d00
Emma Boardman <emma@emmaboardman.co.uk>
2019-04-04 13:58:53 +0100

    Add project files.

---
+++ EmmasCauseShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmmasCauseShop.MvcApplication" Language="C#" %>
---
+++ EmmasCauseShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmmasCauseShop.Startup))]
+namespace EmmasCauseShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
